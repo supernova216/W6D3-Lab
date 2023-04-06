8194fa4cc8d4017927560b0967c5e30855ddfa58
Nathan Harris <nathanharris91@gmail.com>
2023-04-06 16:54:22 -0600

    initial commit

--- public/main.js
+++ public/main.js
@@ -1,5 +1,59 @@
-const btn = document.querySelector('button')
+    const btn = document.querySelector('button')
 
-const clickHandler = () => alert('You are the best!')
+    const clickHandler = () => alert('You are the best!')
 
-btn.addEventListener('click', clickHandler)
+    btn.addEventListener('click', clickHandler)
+
+    const addForm = document.querySelector('form');
+    const nameInput = document.querySelector('input');
+    const container = document.querySelector('section');
+
+    function putTheThingInTheView(res) {
+        container.innerHTML = ''
+        nameInput.value = ''
+
+        res.data.forEach((studentName, index) => {
+            container.innerHTML += `<p name=${index}>${studentName}</p>`
+        })
+
+        document.querySelectorAll('p').forEach(element => {
+            const theIndexValue = element.getAttribute('name');
+
+            element.addEventListener('click', () => {
+                axios
+                    .delete(`http://localhost:4001/api/students/${theIndexValue}`)
+                    .then(res => {
+                        putTheThingInTheView(res)
+                    })
+            })
+        })
+    }
+    function submitHandler(evt) {
+        evt.preventDefault();
+
+        axios
+            .post('http://localhost:4001/api/students', { name: nameInput.value })
+            .then(res => {
+                putTheThingInTheView(res)
+            })
+            .catch(err => {
+                nameInput.value = ''
+
+                const notif = document.createElement('aside')
+                notif.innerHTML = `<p>${err.response.data}</p>
+                <button class="close">close</button>`
+                document.body.appendChild(notif)
+
+                document.querySelectorAll('.close').forEach(btn => {
+                    btn.addEventListener('click', (e) => {
+                        e.target.parentNode.remove()
+                    })
+                })
+            })
+    }
+        axios
+            .get('http://localhost:4001/api/students')
+            .then(res => {
+                putTheThingInTheView(res)
+                })
+            addForm.addEventListener('submit', submitHandler)
--- server.js
+++ server.js
@@ -1,7 +1,39 @@
 const express = require('express')
-const app = express()
+const cors = require('cors')
+const app = express()   
 
 app.use(express.static(`${__dirname}/public`))
+app.use(cors())
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '31c34202fc114feaa3c66ba66e60b654',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+const students = ['Jimmy', 'Timothy', 'Jimothy']
+
+// app.get('/api/students',(req,res)=>{
+// try{
+//   nonexistentFunction()
+//   }
+//   catch (err) {
+//     rollbar.error(err) 
+//   }
+// })
+
+app.get('/api/students', (req, res) => {
+  res.status(200).send(students)
+})
+
+
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+
 
 app.listen(4001,
    () => console.log(`server running on 4001`))
